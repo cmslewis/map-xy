387547628dbb7803e34781cdb63430332e0f8d3d
Chris Lewis <clewis@palantir.com>
2014-10-10 00:31:25 -0700

    Autofill first result. Pull JS out into file.

---
+++ static/js/main.js
@@ -0,0 +1,200 @@
+
+(function() {
+
+  var wasZoomChangeDoneProgrammatically = false;
+
+  var KeyCodes = {
+    TAB   : 9,
+    ENTER : 13
+  };
+
+  var $ui = {
+    panels             : $('.panels'),
+    stackedModeTrigger : $('.js-stacked-mode-trigger'),
+    switchMapsTrigger  : $('.js-switch-maps-trigger'),
+    mapX               : $('.js-map-x'),
+    mapY               : $('.js-map-y'),
+    autocompleteX      : $('.js-autocomplete-x'),
+    autocompleteY      : $('.js-autocomplete-y'),
+    autocompleteFields : $('.search input'),
+    autocompleteLabels : $('.search label')
+  };
+
+  function initialize() {
+    maps = initializeMaps();
+
+    mapX = maps[0]
+    mapY = maps[1]
+
+    initializeButtons(mapX, mapY);
+    initializeSearch(mapX, $ui.autocompleteX, $ui.autocompleteY);
+    initializeSearch(mapY, $ui.autocompleteY);
+
+    $ui.autocompleteX.focus();
+  }
+
+  function propagateZoomLevel(fromMap, toMap) {
+    google.maps.event.addListener(fromMap, 'zoom_changed', function() {
+
+      if (wasZoomChangeDoneProgrammatically) {
+        wasZoomChangeDoneProgrammatically = false;
+      } else {
+        // Change the zoom level in the other map. Be sure to set the flag before changing the zoom of the other map,
+        // because this listener will be fired immediately after the other map's zoom level changes.
+        wasZoomChangeDoneProgrammatically = true;
+        toMap.setZoom(fromMap.getZoom());
+      }
+    });
+  }
+
+  function initializeMaps() {
+    var mapOptions = {
+
+      // Basic Options
+
+      center : { lat: -34.397, lng: 150.644 },
+      zoom   : 8,
+
+      // Enabled Controls
+
+      scaleControl : true,
+
+      // Disabled Controls
+
+      mapTypeControl     : false,
+      panControl         : false,
+      overviewMapControl : false,
+      streetViewControl  : false,
+      zoomControl        : false
+
+    };
+
+    var mapX = new google.maps.Map($ui.mapX[0], mapOptions);
+    var mapY = new google.maps.Map($ui.mapY[0], mapOptions);
+
+    propagateZoomLevel(mapX, mapY);
+    propagateZoomLevel(mapY, mapX);
+
+    return [mapX, mapY];
+  }
+
+  function initializeSearch(map, $input, $nextInput) {
+    var autocompleteService = new google.maps.places.AutocompleteService();
+    var autocomplete        = new google.maps.places.Autocomplete($input[0]);
+    var placesService       = new google.maps.places.PlacesService(map);
+
+    autocomplete.bindTo('bounds', map);
+
+    // We only want to search for coarse, geocode-able locations; not businesses, landmarks, and other small stuff.
+    autocomplete.setTypes(['geocode']);
+
+    // Counterintuitively, the 'place_changed' event is fire not only when the user explicitly selects a place from
+    // the autosuggest menu, but also when they press Enter without selecting a place at all. In the former case, we
+    // can just recenter the map on the autocompleted place. In the latter case, we need to programmatically query
+    // the top few autocomplete results using the string currently in the input field, grab the first result if it
+    // exists, then recenter the map on that.
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+      var place = autocomplete.getPlace();
+
+      if (place.geometry) {
+        recenterMapAndChangeFocus(map, place, $input, $nextInput);
+      } else {
+
+        var queryOptions = {
+          input : $input.val()
+        };
+
+        autocompleteService.getQueryPredictions(queryOptions, function(predictions, status) {
+          if (status !== google.maps.places.PlacesServiceStatus.OK) {
+            return;
+          }
+
+          if (predictions.length === 0) {
+            return;
+          }
+
+          var detailsOptions = {
+            placeId : predictions[0].place_id
+          };
+
+          placesService.getDetails(detailsOptions, function(_place, _status) {
+            if (_status !== google.maps.places.PlacesServiceStatus.OK) {
+              return;
+            }
+            recenterMapAndChangeFocus(map, _place, $input, $nextInput);
+            $input.val(_place.formatted_address);
+          });
+        });
+      }
+    });
+  }
+
+  function initializeButtons(mapX, mapY) {
+
+    $ui.stackedModeTrigger.on('click', function() {
+      if ($ui.panels.attr('data-view-mode') === 'stacked') {
+        $ui.panels.attr('data-view-mode', '');
+        $(this).removeClass('is-impressed');
+      } else {
+        $ui.panels.attr('data-view-mode', 'stacked');
+        $(this).addClass('is-impressed');
+      }
+
+      // We need to let the Maps API know when the size of the Map X container changes.
+      google.maps.event.trigger(mapX, 'resize');
+    });
+
+    $ui.switchMapsTrigger.on('click', function() {
+      var newMapXCenter = mapY.getCenter();
+      var newMapYCenter = mapX.getCenter();
+      mapX.panTo(newMapXCenter);
+      mapY.panTo(newMapYCenter);
+
+      var newAutocompleteXText = $ui.autocompleteY.val();
+      var newAutocompleteYText = $ui.autocompleteX.val();
+      $ui.autocompleteX.val(newAutocompleteXText);
+      $ui.autocompleteY.val(newAutocompleteYText);
+    });
+
+    $ui.autocompleteFields.on('focus', function() { $(this).closest('.input').addClass('focus'); });
+    $ui.autocompleteFields.on('blur',  function() { $(this).closest('.input').removeClass('focus'); });
+
+    $ui.autocompleteLabels.on('click', function() {
+      $ui.autocompleteFields.blur();
+      $(this).siblings('.input').find('input').focus();
+    });
+
+    $ui.autocompleteX.on('keyup', function(ev) {
+      if (ev.keyCode == KeyCodes.ENTER) {
+        console.log('enter in input x');
+      }
+    });
+
+    // Some elements within the maps are visible in the tabbing order. This ensures that tabbing only toggles focus
+    // between the two search fields.
+    $ui.autocompleteY.on('keydown', function(ev) {
+      if (ev.keyCode == KeyCodes.TAB) {
+        ev.preventDefault();
+        $(this).blur();
+        $ui.autocompleteX.focus();
+      }
+    });
+  }
+
+  function recenterMapAndChangeFocus(map, place, $input, $nextInput) {
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } else {
+        map.setCenter(place.geometry.location);
+      }
+
+      $input.blur();
+
+      if ($nextInput != null) {
+        $nextInput.focus();
+      }
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+})();
